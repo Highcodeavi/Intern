5057a2346fc933af5c135ea46d432b269436b7f2
Highcodeavi <bhushanabhinav719@gmail.com>
2023-10-30 15:02:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d))
+  }
+
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  function Userdetails(){
+  }
+
+  function removeData(id){
+    const newData= data.filter(data =>data.id !== id);
+    setData(newData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search People Using ID</h1>
+      <div>
+        <input placeholder="search"></input>
+        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+      </div>
+      <div className="cards" onClick={Userdetails}>
+        {data.map((data) => {
+          return (
+            <div className="card">
+              <p >Id:  {data.id}</p>
+              <p >Name:   {data.name}</p>
+              <p >Username:   {data.username}</p>
+              <p >Email:   {data.email}</p>
+              <p >Phone:   {data.phone}</p>
+              <button onClick={()=> removeData(data.id)}>Remove</button>
+              <button>Edit</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
