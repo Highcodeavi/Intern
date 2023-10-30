0d2d910337d12b42ed0a3cebb9ba2382732b178b
Highcodeavi <92982207+Highcodeavi@users.noreply.github.com>
2023-10-30 15:07:38 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,1 @@
-import React from "react";
 
-function Card({employee}){
-    return(
-        <div>
-            <p>{employee.id}</p>
-            <p>{employee.name}</p>
-            <p>{employee.username}</p>
-            <p>{employee.email}</p>
-            <p>{employee.phone}</p>
-        </div>
-    )
-}
-
-export default Card;
